d10667b841146cada2b1f6151544b1587767bb4c
Majonima <majonima@gmail.com>
2021-07-29 15:05:39 +0300

    algorithm test

--- Main/src/components/HelloWorld.vue
+++ Main/src/components/HelloWorld.vue
@@ -17,39 +17,48 @@ export default {
 let boo= new PowerStation("boo", 10, 13);
 let foo= new PowerStation("foo", 12, 16);
 
-let consumption = [8, 10,14,24,22,11,9,8,15,17,27,10,7,5]
+let consumption = [];
+for (let i=0; i<24;i++){
+  consumption[i]=Math.round(Math.random()*20);
+}
+console.log(consumption)
 
-console.log(boo)
-console.log(foo)
 PowerCalc(consumption,[boo,foo])
 function PowerCalc(consumption, powerstations){
-
+  let inbalance=[];
   powerstations.sort((a,b)=>a.price-b.price);
-  console.log(powerstations)
   for (const lvl in consumption){
 let sumPower=0;
+
 for (const station in powerstations){
   if(sumPower<consumption[lvl]){
-    if(sumPower+powerstations[station].power>consumption[lvl]){
+    if(sumPower+powerstations[station].gen[lvl]>consumption[lvl]){
       powerstations[station].load[lvl]=consumption[lvl]-sumPower;
       powerstations[station].money+=powerstations[station].load[lvl]*powerstations[station].price
       sumPower+=powerstations[station].load[lvl]
     }else{
-      powerstations[station].load[lvl]=powerstations[station].power;
+      powerstations[station].load[lvl]=powerstations[station].gen[lvl];
       powerstations[station].money+=powerstations[station].load[lvl]*powerstations[station].price;
       sumPower+=powerstations[station].load[lvl]
     }
   }
 }
+inbalance[lvl]=consumption[lvl]-sumPower;
   }
   console.log(powerstations)
+  console.log(inbalance)
 }
 function PowerStation(name, price, power){
   this.name=name;
   this.price=price;
   this.power=power;
+  this.faultChance=0.2;
   this.money=0;
   this.load=[];
+  this.gen=[];
+  for(let i=0; i<24;i++){
+      this.gen[i]=  Math.random()>this.faultChance? this.power: 0;
+  }
   //this.price=Math.random()*10;
   //this.power=10+Math.random()*10;
   return this
